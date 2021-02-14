af9770011cf4ae3ff175f10e6097f0aa8ce799cd
Karthick Laptop <smekarthick2005@gmail.com>
2021-02-14 11:21:46 +0530

    Lesson 3 completed

--- krandom/krandom/urls.py
+++ krandom/krandom/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path(r'^$', include('boards.urls'))
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
 ]
